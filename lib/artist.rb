87a3416955aa6fd1f52a8922004d3ff96309394a
Jonathan East <jonathan_east@ymail.com>
2020-05-24 23:43:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,7 @@ class Artist
   end
 
   def add_song_by_name(song_name)
-    song = Song.new
-    song.name = song_name
-    song.artist = self
+    song = Song.new(song_name)
+    add_song(song)
   end
 end
